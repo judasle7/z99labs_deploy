accfe5455c8a20c9faac94bda464556d5d2531b9
judasle7 <judasle7@gmail.com>
2024-11-09 14:47:50 +0700

    new

---
+++ _nuxt/tubgv-F3.js
@@ -0,0 +1,1 @@
+import{ab as e,m as t}from"./ckVKJ7eS.js";const l=e((a,n)=>{{const o=localStorage.getItem("token");return console.log(o),!o||o!=="validToken"?t("/login"):void 0}});export{l as default};

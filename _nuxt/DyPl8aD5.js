d2cdd4a308b8b8f4f12585fe0d3fb95a24af0865
judasle7 <judasle7@gmail.com>
2024-11-09 14:42:33 +0700

    F

---
+++ _nuxt/DyPl8aD5.js
@@ -0,0 +1,1 @@
+import{ab as e,m as t}from"./IgMk9t1Q.js";const l=e((a,n)=>{{const o=localStorage.getItem("token");return console.log(o),!o||o!=="validToken"?t("/login"):void 0}});export{l as default};
